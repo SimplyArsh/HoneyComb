a849ec43e72cdecfe5ccc32b161a5b85c7a69609
Itisalex2 <chenalexlu@gmail.com>
2023-11-08 23:30:08 -0800

    Added comments to backend/server.js. Created controllers for the backend routes and changed example to incorportate controllers. Added package.json and package-lock.json for the backend.

--- backend/routes/example.js
+++ backend/routes/example.js
@@ -1,33 +1,10 @@
 const router = require('express').Router()
-let Example = require('../models/example.model');
+const { getExamples, addExample } = require('../controllers/example-controller')
 
 
-router.route('/').get((req, res) => {
-  Example.find()
-    .then(examples => res.json(examples))
-    .catch(err => res.status(400).json('Error: ' + err));
-});
+router.get('/', getExamples);
 
-
-router.route('/add').post((req, res) => {
-  const name = req.body.name;
-  const description = req.body.description;
-  const date = Date.parse(req.body.date);
-
-
-  const newExample = new Example({
-    name,
-    description,
-    date,
-  });
-
-
-  newExample.save()
-    .then(() => res.json('Example added!'))
-    .catch(err => res.status(400).json('Error: ' + err));
-});
+router.post('/add', addExample);
 
 
 module.exports = router;
-
-;
--- backend/server.js
+++ backend/server.js
@@ -5,24 +5,26 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-const examplesRouter = require('./routes/example')
+const examplesRouter = require('./routes/example');
 
-
+// Middleware
 app.use(cors());
 app.use(express.json());
+
+// Routers
 app.use('/examples', examplesRouter);
 
-
+// Connect to MongoDB
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+mongoose.connect(uri);
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 });
 
-
+// Start backend server
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
 
-;
+
