a976b3b2cef47a7d23fc8a6f60aeb35f6ca37db4
Itisalex2 <chenalexlu@gmail.com>
2023-11-09 15:46:14 -0800

    Added refsheet and profile page template
    Added font awesome refsheet into index.html. Designed basic profile page.

--- frontend/src/views/Profile.js
+++ frontend/src/views/Profile.js
@@ -1,7 +1,43 @@
+import { useState } from "react"
+import { Link } from 'react-router-dom'
+
+const ProfileUserInfo = ({ username, dateJoined, likes, posts, aboutMe }) => (
+    <div className="header">
+        <div className="icon">
+            <i className="fa fa-user"></i> {/* Font Awesome icons */}
+        </div>
+        <div className="profileInfo">
+            <h1>{username}</h1>
+            <p>Date Joined: {dateJoined}</p>
+            <p>Likes: {likes}</p>
+            <p>Posts: {posts}</p>
+            <p>About me: {aboutMe}</p>
+        </div>
+        <div className="actions">
+            <Link to="/createProject"><i className="fa fa-plus"></i></Link>
+            <Link to="/messages"><i className="fa fa-envelope"></i></Link>
+        </div>
+    </div>
+);
+
+const PostList = ({ posts, title }) => (
+    <div className="postList">
+        <h2>{title}</h2>
+        {/* {posts.map((post, index) => (
+            <div key={index} className="post">
+                <h3>{post.title}</h3>
+                <p>{post.content}</p>
+            </div>
+        ))} */}
+    </div>
+);
+
 const Profile = () => {
     return (
-        <div className="createProfile">
-            <h1>Profile</h1>
+        <div className="profilePage">
+            <ProfileUserInfo />
+            <PostList title="Current Posts" />
+            <PostList title="Past Posts" />
         </div>
     )
 }
